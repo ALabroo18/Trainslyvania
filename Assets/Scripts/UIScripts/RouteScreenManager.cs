7b0c3d699fbe06f11bf5552b1bd4f78a28d7c375
Bernard Fonrose <bernardthebear19@gmail.com>
2026-03-16 02:45:46 -0400

    Shop UI & Win Screen

---
+++ Assets/Scripts/UIScripts/RouteScreenManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RouteScreenManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
