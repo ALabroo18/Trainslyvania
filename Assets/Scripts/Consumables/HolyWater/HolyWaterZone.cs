f6ffef0e8cbc49ba46cd4c0149b41fd58c492dd2
Bernard Fonrose <bernardthebear19@gmail.com>
2026-04-22 16:07:30 -0400

    VFX changes
    Fixed z fighting and fire bomb now spawns

--- Assets/Scripts/Consumables/HolyWater/HolyWaterZone.cs
+++ Assets/Scripts/Consumables/HolyWater/HolyWaterZone.cs
@@ -36,6 +36,8 @@ public class HolyWaterZone : MonoBehaviour
         {
             shaderEffect.SetFloat("Zone Size", radius * 2);
             shaderEffect.SetFloat("Zone Lifetime", duration);
+            Vector3 newPos = new Vector3(transform.position.x, .02f ,transform.position.z);
+            gameObject.transform.position = newPos;
             shaderEffect.Play();
            
         }
@@ -78,6 +80,7 @@ public class HolyWaterZone : MonoBehaviour
                 if (health != null)
                 {
                     health.TakeDamage(damagePerTick);
+                    Debug.Log("Taking DOT tick");
                     if (!tintedVampires.ContainsKey(col.gameObject))
                     {
                         Coroutine tintRoutine = StartCoroutine(TintVampire(col.gameObject));
